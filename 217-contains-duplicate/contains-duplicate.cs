b634e58de101ae01db050e964a863121057914c8
Emelie Hälgh <emeliecaroline99@gmail.com>
2026-06-02 13:36:57 +0200

    Time: 14 ms (39.63%) | Memory: 67.2 MB (71.01%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+
+        HashSet<int> seen = new HashSet<int>();
+
+        for (int i=0; i < nums.Length; i++)
+        {
+            if(seen.Contains(nums[i]))
+            {
+                return true;
+            }
+            else
+            {
+                seen.Add(nums[i]);
+            }
+        }
+
+        return false;
+        
+    }
+}
