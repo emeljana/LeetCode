6f69a33ee845fe5687aa4599a83df34a4754db3a
Emelie Hälgh <emeliecaroline99@gmail.com>
2026-09-02 20:12:23 +0200

    Time: 536 ms (6.89%) | Memory: 65.5 MB (32.21%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -5,14 +5,18 @@
 var maxProfit = function(prices) {
     profit = 0;
     lowest = 100000
+    highest = 0;
     for (i=0; i<prices.length-1; i++){
         if (prices[i]<lowest){
             lowest = prices[i];
             for (j=i+1; j<=prices.length; j++){
-            currentProfit = prices[j]-prices[i];
-            if (currentProfit > profit) {
-                profit = currentProfit;
-            }
+                if(prices[j]>highest){
+                    currentProfit = prices[j]-prices[i];
+                    if (currentProfit > profit) {
+                        profit = currentProfit;
+                    }
+                }
+            
         }
         }
     }
